622c8bc7f6c23c2565424d607bb511e65b8e447e
Muhammad Rivaldi Fanani <muhammadrivaldifnni01@gmail.com>
2025-12-20 23:27:32 +0700

    done: backup telegram

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('db:backup-telegram')
+    ->dailyAt('23.23')
+    ->timezone('Asia/Jakarta');
+
+
